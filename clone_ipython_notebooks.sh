55c818b6f0b5176325a7b16b69f635fb3c429b7a
George Zero <zero@trove.nyc>
2016-11-19 09:36:40 -0500

    added pkmital tf tutorials to clone

--- clone_ipython_notebooks.sh
+++ clone_ipython_notebooks.sh
@@ -5,6 +5,7 @@ git clone https://github.com/jakevdp/WhirlwindTourOfPython.git
 git clone https://github.com/jakevdp/PythonDataScienceHandbook.git
 git clone https://github.com/ipython-books/minibook-2nd-code.git
 git clone https://github.com/pkmital/CADL.git
+git clone https://github.com/pkmital/tensorflow_tutorials.git
 git clone https://bitbucket.org/hrojas/learn-pandas.git
 git clone https://github.com/jdwittenauer/ipython-notebooks.git
 git clone https://github.com/gumption/Python_for_Data_Science.git
